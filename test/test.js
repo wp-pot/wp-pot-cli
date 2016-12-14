b6682247bb5ca66b3af23c492d9c22ee41f8b904
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-12-14 09:57:26 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -6,15 +6,17 @@ const execa = require('execa');
 const assert = require('assert');
 
 describe('Test CLI output', function () {
-  it('should output pot strings', function () {
+  it('should output pot strings', function (done) {
     execa.shell('./cli.js --src test/fixtures/comments.php').then(function (result) {
-      assert(true, result.indexOf('"X-Poedit-SourceCharset: UTF-8\n"') !== -1);
-      assert(true, result.indexOf('Comment with other keyword') !== -1);
+      assert(true, result.stdout.indexOf('"X-Poedit-SourceCharset: UTF-8\n"') !== -1);
+      assert(true, result.stdout.indexOf('Comment with other keyword') !== -1);
+      done();
     });
   });
-  it('should output error when destination file is empty and write file is true', function () {
-    execa.shell('./cli.js --write-file true').then(function (result) {
+  it('should output error when destination file is empty and write file is true', function (done) {
+    execa.shell('./cli.js --write-file true').catch(function (result) {
       assert('Destination file flag is empty', result);
+      done();
     });
   });
 });
